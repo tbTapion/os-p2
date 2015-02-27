92aecc6f5d7ceff007beb1786fde6738cf0b4d18
Andreas <tb.tapion@gmail.com>
2015-02-27 12:13:37 +0100

    Fixed doorman, he should be ready for action

--- Doorman.java
+++ Doorman.java
@@ -2,29 +2,48 @@
  * This class implements the doorman's part of the
  * Barbershop thread synchronization example.
  */
-public class Doorman {
+public class Doorman extends Thread{
+
+	private CustomerQueue queue;
+	private Gui gui;
+	private boolean running;
+
 	/**
 	 * Creates a new doorman.
 	 * @param queue		The customer queue.
 	 * @param gui		A reference to the GUI interface.
 	 */
-	public Doorman(CustomerQueue queue, Gui gui) { 
-		// Incomplete
+	public Doorman(CustomerQueue queue, Gui gui) {
+		this.queue = queue;
+		this.gui = gui;
 	}
 
 	/**
 	 * Starts the doorman running as a separate thread.
 	 */
 	public void startThread() {
-		// Incomplete
+		boolean = false;
 	}
 
 	/**
 	 * Stops the doorman thread.
 	 */
 	public void stopThread() {
-		// Incomplete
+		boolean = true;
+		this.run();
 	}
 
 	// Add more methods as needed
+	@Override
+	private void run(){
+		while(running){
+			try{
+				this.sleep((long)(Math.random()*Globals.doormanSleep));
+			}catch(Exception E){
+				e.printStackTrace();
+			}
+			gui.println("Doorman: Trying to get a new customer...");
+			queue.addCustomer();
+		}
+	}
 }
